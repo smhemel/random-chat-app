945afbd8ba69171721b9925157ed2b3bd6f1e0ad
smhemel <smhemel.eu@gmail.com>
2024-05-25 16:11:18 +0600

    Added chat message save functionality

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,17 +1,45 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ChatService } from '../../services/chat/chat.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css',
 })
 export class ChatComponent {
-  private auth = inject(AuthService);
   private router = inject(Router);
+  private auth = inject(AuthService);
+  private chatService = inject(ChatService);
+  private formBuilder = inject(FormBuilder);
+
+  chatForm!: FormGroup;
+
+  constructor() {
+    this.chatForm = this.formBuilder.group({
+      chat_message: ['', Validators.required],
+    });
+  }
+
+  async onSubmit() {
+    this.chatService
+      .chatMessage(this.chatForm.value.chat_message)
+      .then((res) => {
+        alert('message successfully saved.');
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  }
 
   async logOut() {
     this.auth
---
+++ src/app/services/chat/chat.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatService {
+  private supabase!: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+  }
+
+  async chatMessage(message: string): Promise<any> {
+    try {
+      const { data, error } = await this.supabase
+        .from('chat')
+        .insert({ message });
+
+      if (error) {
+        alert(error.message);
+      }
+
+      return data;
+    } catch (e: any) {
+      alert(e.message);
+    }
+  }
+}
